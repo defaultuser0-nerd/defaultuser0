b11b51399b6ace4fd14904450ce5baf600248fc3
defaultuser0 <axolotlslmao@gmail.com>
2024-11-24 21:14:20 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,4 +1,4 @@
-﻿namespace Grate
+namespace Grate
 {
     /// <summary>
     /// This class is used to provide information about your mod to BepInEx.
@@ -6,7 +6,7 @@
     internal class PluginInfo
     {
         public const string GUID = "com.kylethescientist.graze.gorillatag.Grate";
-        public const string Name = "Grate";
-        public const string Version = "1.5.8"; 
+        public const string Name = "defaultbark0";
+        public const string Version = "2.0.0"; 
     }
 }
